8a372ae01e7946cf8a229b5f0ee86d7e181bfc25
gnix22 <gjnix22@gmail.com>
2025-06-30 22:37:16 -0400

    update: got projects prettyish

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,20 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const snippetHead = document.querySelector(".snippet-heading")
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    snippetHead.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    snippetHead.remove("active");
 }))
+
+function revealText(section) {
+  console.log("Clicked heading:", section);
+  section.classList.toggle('revealed');
+}
